2d3f3926e1a6dc1d9173f70ab992725323087f53
Tamal Kumar De <tamalkumar.de@kuleuven.be>
2019-07-25 07:10:15 +0000

    version 1.3.0

--- R/ES.R
+++ R/ES.R
@@ -6,6 +6,9 @@ function(design,ES,data=read.table(file.choose(new=FALSE))){
     no_overlap=0
     comparisons=length(A)*length(B)
     
+    if(comparisons == 0)
+      return(NA)
+    
     for (j in 1: length(A)){
       for (k in 1:length(B)){
         if(A[j]<B[k]){
@@ -29,6 +32,10 @@ function(design,ES,data=read.table(file.choose(new=FALSE))){
       A<-c(data[,2][data[,1]=="A1"],data[,2][data[,1]=="A2"])
       B<-data[,2][data[,1]=="B1"]
     }
+    
+    A <- as.vector(na.omit(A))
+    B <- as.vector(na.omit(B))
+    
     if(ES=="SMD"){
       es<-(mean(B)-mean(A))/sd(A) 
     }
@@ -60,6 +67,11 @@ function(design,ES,data=read.table(file.choose(new=FALSE))){
     B1<-data[,2][data[,1]=="B1"]
     A2<-data[,2][data[,1]=="A2"]
     B2<-data[,2][data[,1]=="B2"]
+    
+    A1 <- as.vector(na.omit(A1))
+    B1 <- as.vector(na.omit(B1))
+    A2 <- as.vector(na.omit(A2))
+    B2 <- as.vector(na.omit(B2))
   
     if(ES=="SMD"){
       es1<-(mean(B1)-mean(A1))/sd(A1)
@@ -93,7 +105,7 @@ function(design,ES,data=read.table(file.choose(new=FALSE))){
       es1<-1-NAP_calc(A1,B1)
       es2<-1-NAP_calc(A2,B2)
     }
-    es<-mean(c(es1,es2))
+    es<-mean(c(es1,es2),na.rm=TRUE)
   }
   
   if(design=="MBD"){
@@ -103,6 +115,9 @@ function(design,ES,data=read.table(file.choose(new=FALSE))){
     for(it in 1:N){
       A<-data[,it*2][data[,(it*2)-1]=="A"]	
       B<-data[,it*2][data[,(it*2)-1]=="B"]
+      
+      A <- as.vector(na.omit(A))
+      B <- as.vector(na.omit(B))
       
       if(ES=="SMD"){
         effectsizes[it]<-(mean(B)-mean(A))/sd(A) 
@@ -129,7 +144,7 @@ function(design,ES,data=read.table(file.choose(new=FALSE))){
         effectsizes[it]<-1-NAP_calc(A,B)
       }
     } 
-    es<-mean(effectsizes)
+    es<-mean(effectsizes,na.rm=TRUE)
   }
   
   return(es)
